2937335d75e7a08dafcfac990674bc13f571dbf6
Nuri <nuriidyliz@gmal.com>
2019-05-03 22:25:13 +0300

    execution calculation is done * 1k, 2k, 10k, 20k, 30k input size * ascending, descending, randomized order input sets

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import time
-arr1 = []
-print(len(arr1))
-# for i in range(0,5):
-#     arr1[i] = i
+import sys
+sys.setrecursionlimit(2001)
 def insertionSort(arr):
     for i in range(1, len(arr)):
         key = arr[i]
@@ -65,7 +63,6 @@ def mergeSort(arr, l, r):
 def partition(arr, low, high):
     i = (low - 1)  # index of smaller element
     pivot = arr[high]  # pivot
-
     for j in range(low, high):
 
         # If current element is smaller than or
@@ -123,15 +120,18 @@ def heapSort(arr):
         heapify(arr, i, 0)
 def countSort(arr):
     # The output character array that will have sorted arr
-    output = [0 for i in range(256)]
+    output = [0 for i in range(len(arr))]
 
     # Create a count array to store count of inidividul
     # characters and initialize count array as 0
-    count = [0 for i in range(256)]
+    count = [0 for i in range(1001)]
 
 
     # Store count of each character
+
     for i in arr:
+        if type(i) == str:
+            print(i);
         count[i] += 1
 
     # Change count[i] so that count[i] now contains actual
@@ -149,10 +149,131 @@ def countSort(arr):
     for i in range(len(arr)):
         arr[i] = output[i]
 
-n = len(arr1)
-startTime = time.time()
-quickSort(arr1,0,n-1)
-endTime = time.time()
-mTime = endTime - startTime
-print(arr1)
-print ("Time measurement " + str(mTime))
+smallAscText = open("Sets/1k_Ascending.txt","r")
+smallDescText = open("Sets/1k_Descending.txt","r")
+smallRandText = open("Sets/1k_Random.txt","r")
+smallAscText2 = open("Sets/2k_Ascending.txt","r")
+smallDescText2 = open("Sets/2k_Descending.txt","r")
+smallRandText2 = open("Sets/2k_Random.txt","r")
+mediumAscText = open("Sets/10k_Ascending.txt","r")
+mediumDescText = open("Sets/10k_Descending.txt","r")
+mediumRandText = open("Sets/10k_Random.txt","r")
+largeAscText = open("Sets/20k_Ascending.txt","r")
+largeDescText = open("Sets/20k_Descending.txt","r")
+largeRandText = open("Sets/20k_Random.txt","r")
+xLargeAscText = open("Sets/30k_Ascending.txt","r")
+xLargeDescText = open("Sets/30k_Descending.txt","r")
+xLargeRandText = open("Sets/30k_Random.txt","r")
+
+smallAscArr =smallAscText.read().split(',')
+smallAscArr = list(map(int,smallAscArr))
+
+smallDescArr =smallDescText.read().split(',')
+smallDescArr = list(map(int,smallDescArr))
+
+smallRandArr =smallRandText.read().split(',')
+smallRandArr = list(map(int,smallRandArr))
+
+smallAscArr2 =smallAscText2.read().split(',')
+smallAscArr2 = list(map(int,smallAscArr2))
+
+smallDescArr2 =smallDescText2.read().split(',')
+smallDescArr2 = list(map(int,smallDescArr2))
+
+smallRandArr2 =smallRandText2.read().split(',')
+smallRandArr2 = list(map(int,smallRandArr2))
+
+mediumAscArr =mediumAscText.read().split(',')
+mediumAscArr = list(map(int,mediumAscArr))
+
+mediumDescArr =mediumDescText.read().split(',')
+mediumDescArr = list(map(int,mediumDescArr))
+
+mediumRandArr =mediumRandText.read().split(',')
+mediumRandArr = list(map(int,mediumRandArr))
+
+largeAscArr =largeAscText.read().split(',')
+largeAscArr = list(map(int,largeAscArr))
+
+largeDescArr =largeDescText.read().split(',')
+largeDescArr = list(map(int,largeDescArr))
+
+largeRandArr =largeRandText.read().split(',')
+largeRandArr = list(map(int,largeRandArr))
+
+xLargeAscArr = xLargeAscText.read().split(',')
+xLargeAscArr = list(map(int,xLargeAscArr))
+
+xLargeDescArr = xLargeDescText.read().split(',')
+xLargeDescArr = list(map(int,xLargeDescArr))
+
+xLargeRandArr = xLargeRandText.read().split(',')
+xLargeRandArr = list(map(int,xLargeRandArr))
+
+smallAscText.close()
+smallDescText.close()
+smallRandText.close()
+smallAscText2.close()
+smallDescText2.close()
+smallRandText2.close()
+mediumAscText.close()
+mediumDescText.close()
+mediumRandText.close()
+largeAscText.close()
+largeDescText.close()
+largeRandText.close()
+xLargeAscText.close()
+xLargeDescText.close()
+xLargeRandText.close()
+
+setArr = [smallAscArr,smallDescArr,smallRandArr, smallAscArr2, smallDescArr2, smallRandArr2,
+          mediumAscArr,mediumDescArr,mediumRandArr,largeAscArr,largeDescArr, largeRandArr,
+          xLargeAscArr, xLargeDescArr, xLargeRandArr]
+setName = ["1k_Ascending", "1k_Descending", "1k_Random",
+           "2k_Ascending", "2k_Descending", "2k_Random",
+           "10k_Ascending", "10k_Descending", "10k_Random",
+           "20k_Ascending", "20k_Descending", "20k_Random",
+           "30k_Ascending", "30k_Descending", "30k_Random"]
+print(setArr[8])
+quickSort(setArr[8], 0 , len(setArr[8]) -1)
+print(setArr[8])
+
+for i in range(0, len(setArr)):
+    print("-----Set: "+  setName[i] +" Time Measurements-----")
+    print(setArr[i])
+    startTime = time.time()
+    insertionSort(setArr[i])
+    endTime = time.time()
+    execTime = (endTime - startTime) * 100
+    print("Insertion Sort Execution Time: ", str(execTime))
+
+    startTime = time.time()
+    mergeSort(setArr[i], 0 , len(setArr[i]) -1)
+    endTime = time.time()
+    execTime = (endTime - startTime) * 100
+    print("Merge Sort Execution Time: ", str(execTime))
+
+    try :
+        startTime = time.time()
+        quickSort(setArr[i], 0 , len(setArr[i]) -1)
+        endTime = time.time()
+        execTime = (endTime - startTime) * 100
+        print("Quick Sort Execution Time: ", str(execTime))
+
+    except RecursionError:
+        print("Quick Sort Recursion Error")
+
+
+    startTime = time.time()
+    heapSort(setArr[i])
+    endTime = time.time()
+    execTime = (endTime - startTime) * 100
+    print("Heap Sort Execution Time: ", str(execTime))
+
+    startTime = time.time()
+    countSort(setArr[i])
+    endTime = time.time()
+    execTime = (endTime - startTime) * 100
+    print("Count Sort Execution Time: ", str(execTime))
+
+    print()
